d6fc287207271597169e1e9f78cb681edb1b0090
Himanshu Verma <vhimanshu0817@gmail.com>
2025-11-14 23:20:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -61,6 +61,12 @@ app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use('/api/auth', userRoute);
 app.use('/api/messages', messageRouter);
 
+app.get("/", (req, res) => {
+  res.send("Quick Chat Backend is running...");
+});
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+
 // ✅ Start the HTTP server (this is essential for Socket.IO)
 if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5000;
